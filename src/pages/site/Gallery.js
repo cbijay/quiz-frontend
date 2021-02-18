e5bda695f8ac454c203c3e01cf670fa5febe632f
Saurab Gami <saurabgami977@gmail.com>
2021-02-18 14:43:19 +0545

    image import file separated

--- src/pages/site/Gallery.js
+++ src/pages/site/Gallery.js
@@ -1,66 +1,8 @@
 import { Card, CardContent, Grid, makeStyles, Modal, Typography } from '@material-ui/core'
 import React, { useState } from 'react'
 import CardHeading from '../../components/card/CardHeading'
-import SiteLayout from '../../layouts/SiteLayout'
-
-import Banner1 from "../../components/home/images/banner.jpg";
-import Banner3 from "../../components/home/images/banner3.jpg";
-import Banner4 from "../../components/home/images/gallery1.jpg";
-import Banner5 from "../../components/home/images/gallery2.jpg";
-import Banner6 from "../../components/home/images/gallery3.jpg";
-import Banner7 from "../../components/home/images/gallery4.jpg";
-import Banner8 from "../../components/home/images/gallery5.jpg";
-import Banner9 from "../../components/home/images/gallery6.jpg";
-import Banner10 from "../../components/home/images/gallery7.jpg";
-import SchoolPhoto1 from "../../images/School Photo1.jpg";
-import SchoolPhoto2 from "../../images/School Photo2.jpg";
-import SchoolPhoto3 from "../../images/School Photo3.png";
-import SchoolPhoto4 from "../../images/School Photo 4.jpg";
-import SchoolPhoto5 from "../../images/School Photo 5.jpg";
-import SchoolPhoto7 from "../../images/School Photo 7.png";
-import SchoolPhoto8 from "../../images/School Photo 8.jpg";
-import SchoolPhoto9 from "../../images/School Photo 9.png";
-import SchoolPhoto10 from "../../images/School Photo 10.png";
-import SchoolPhoto12 from "../../images/School Photo 12.png";
-import SchoolPhoto13 from "../../images/School Photo 13.jpg";
-import SchoolPhoto14 from "../../images/School Photo 14.jpg";
-import SchoolPhoto15 from "../../images/School Photo 15.jpg";
-import SchoolPhoto16 from "../../images/School Photo 16.jpg";
-import SchoolPhoto17 from "../../images/School Photo 17.png";
-import SchoolPhoto18 from "../../images/School Photo 18.png";
-import SchoolPhoto19 from "../../images/School Photo 19.jpg";
-import SchoolPhoto20 from "../../images/School Photo 20.jpg";
-import SchoolPhoto21 from "../../images/School Photo 21.jpg";
-import SchoolPhoto23 from "../../images/School Photo 23.jpg";
-import SchoolPhoto24 from "../../images/School Photo 24.jpg";
-import SchoolPhoto26 from "../../images/School Photo 26.jpg";
-import SchoolPhoto27 from "../../images/School Photo 27.jpg";
-import SchoolPhoto28 from "../../images/School Photo 28.jpg";
-import SchoolPhoto29 from "../../images/School Photo 29.jpg";
-import SchoolPhoto30 from "../../images/School Photo 30.jpg";
-import SchoolPhoto31 from "../../images/School Photo 31.jpg";
-import SchoolPhoto32 from "../../images/School Photo 32.jpg";
-import SchoolPhoto33 from "../../images/School Photo 33.jpg";
-import SchoolPhoto34 from "../../images/School Photo 34.jpg";
-import SchoolPhoto35 from "../../images/School Photo 35.jpg";
-import SchoolPhoto36 from "../../images/School Photo 36.jpg";
-import SchoolPhoto37 from "../../images/School Photo 37.jpg";
-import SchoolPhoto38 from "../../images/School Photo 38.jpg";
-import SchoolPhoto39 from "../../images/School Photo 39.jpg";
-import SchoolPhoto40 from "../../images/School Photo 40.jpg";
-import SchoolPhoto41 from "../../images/School Photo 41.jpg";
-import SchoolPhoto43 from "../../images/School Photo 43.jpg";
-import SchoolPhoto45 from "../../images/School Photo 45.jpg";
-import SchoolPhoto46 from "../../images/School Photo 46.jpg";
-import SchoolPhoto47 from "../../images/School Photo 47.jpg";
-import SchoolPhoto48 from "../../images/School Photo 48.jpg";
-import SchoolPhoto49 from "../../images/School Photo 49.jpg";
-import SchoolPhoto50 from "../../images/School Photo 50.jpg";
-import SchoolPhoto51 from "../../images/School Photo 51.jpg";
-import SchoolPhoto52 from "../../images/School Photo 52.jpg";
-import SchoolPhoto53 from "../../images/School Photo 53.jpg";
-import SchoolPhoto54 from "../../images/School Photo 54.jpg";
-import SchoolPhoto121 from "../../images/School Photo 121.png";
+import SiteLayout from '../../layouts/SiteLayout';
+import { images } from './Images'
 
 const styles = makeStyles(theme => ({
     image: {
@@ -106,66 +48,7 @@ const Gallery = () => {
     const classes = styles();
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [image, setImage] = useState(null);
-    const images = [
-        { src: Banner1, description: 'Dance competition' },
-        { src: Banner3, description: 'Join our yoga class' },
-        { src: Banner4, description: 'This is Image Description' },
-        { src: Banner5, description: 'This is Image Description' },
-        { src: Banner6, description: 'This is Image Description' },
-        { src: Banner7, description: 'This is Image Description' },
-        { src: Banner8, description: 'This is Image Description' },
-        { src: Banner9, description: 'This is Image Description' },
-        { src: Banner10, description: 'This is Image Description' },
-        { src: SchoolPhoto1, description: 'This is Image Description' },
-        { src: SchoolPhoto2, description: 'This is description' },
-        { src: SchoolPhoto3, description: 'This is description' },
-        { src: SchoolPhoto4, description: 'This is description' },
-        { src: SchoolPhoto5, description: 'This is description' },
-        { src: SchoolPhoto7, description: 'This is description' },
-        { src: SchoolPhoto8, description: 'This is description' },
-        { src: SchoolPhoto9, description: 'This is description' },
-        { src: SchoolPhoto10, description: 'This is description' },
-        { src: SchoolPhoto12, description: 'This is description' },
-        { src: SchoolPhoto13, description: 'This is description' },
-        { src: SchoolPhoto14, description: 'This is description' },
-        { src: SchoolPhoto15, description: 'This is description' },
-        { src: SchoolPhoto16, description: 'This is description' },
-        { src: SchoolPhoto17, description: 'This is description' },
-        { src: SchoolPhoto18, description: 'This is description' },
-        { src: SchoolPhoto19, description: 'This is description' },
-        { src: SchoolPhoto20, description: 'This is description' },
-        { src: SchoolPhoto21, description: 'This is description' },
-        { src: SchoolPhoto23, description: 'This is description' },
-        { src: SchoolPhoto24, description: 'This is description' },
-        { src: SchoolPhoto26, description: 'This is description' },
-        { src: SchoolPhoto27, description: 'This is description' },
-        { src: SchoolPhoto28, description: 'This is description' },
-        { src: SchoolPhoto29, description: 'This is description' },
-        { src: SchoolPhoto30, description: 'This is description' },
-        { src: SchoolPhoto31, description: 'This is description' },
-        { src: SchoolPhoto32, description: 'This is description' },
-        { src: SchoolPhoto33, description: 'This is description' },
-        { src: SchoolPhoto34, description: 'This is description' },
-        { src: SchoolPhoto35, description: 'This is description' },
-        { src: SchoolPhoto36, description: 'This is description' },
-        { src: SchoolPhoto37, description: 'This is description' },
-        { src: SchoolPhoto38, description: 'This is description' },
-        { src: SchoolPhoto39, description: 'This is description' },
-        { src: SchoolPhoto40, description: 'This is description' },
-        { src: SchoolPhoto41, description: 'This is description' },
-        { src: SchoolPhoto43, description: 'This is description' },
-        { src: SchoolPhoto45, description: 'This is description' },
-        { src: SchoolPhoto46, description: 'This is description' },
-        { src: SchoolPhoto47, description: 'This is description' },
-        { src: SchoolPhoto48, description: 'This is description' },
-        { src: SchoolPhoto49, description: 'This is description' },
-        { src: SchoolPhoto50, description: 'This is description' },
-        { src: SchoolPhoto51, description: 'This is description' },
-        { src: SchoolPhoto52, description: 'This is description' },
-        { src: SchoolPhoto53, description: 'This is description' },
-        { src: SchoolPhoto54, description: 'This is description' },
-        { src: SchoolPhoto121, description: 'This is description' },
-    ]
+
 
     const modalCloseHandler = () => {
         setImage(null)
